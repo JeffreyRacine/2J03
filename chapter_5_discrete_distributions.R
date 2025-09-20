1a2dd782733856e583d3a439821022f7c8f4789e
Jeffrey Racine <racinej@mcmaster.ca>
2025-09-20 11:47:32 -0400

    Binomial example

--- chapter_5_discrete_distributions.R
+++ chapter_5_discrete_distributions.R
@@ -11,3 +11,26 @@ var_x <- sum((x - mean_x)^2 * p)
 sd_x <- sqrt(var_x)
 mean_x
 sd_x
+
+## Binomial distribution, how to compute n choose x in R, and given pi, the
+## probability of success, how to use this to compute p(x). Consider a simple
+## example.
+
+n <- 10
+x <- 3
+
+choose(n, x)
+
+## Now let pi be the probability of success
+
+pi <- 0.2
+
+## Compute p(x) using the formula for the binomial distribution
+
+p_x <- choose(n, x) * pi^x * (1 - pi)^(n - x)
+p_x
+
+## Use the R function dbinom to compute p(x) for a binomial distribution
+
+p_x <- dbinom(x, size = n, prob = pi)
+p_x
