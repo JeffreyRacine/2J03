88001859cd92506f3d4da1104c6d2c2284c436fc
Jeffrey Racine <racinej@mcmaster.ca>
2025-09-30 12:27:38 -0400

    Adding to Ch 5

--- chapter_5_discrete_distributions.R
+++ chapter_5_discrete_distributions.R
@@ -1,10 +1,17 @@
-## Let x denote the number of defective computer parts in a shipment of 400. The
+## Let X denote the number of breakdowns for this machine during a given week. 
+
+x <- c(0, 1, 2, 3)
+P <- c(0.15, 0.20, 0.35, 0.30)
+
+sum(x * P)
+
+## Let X denote the number of defective computer parts in a shipment of 400. The
 ## following table gives the probability distribution of x.
 
 x <- c(0, 1, 2, 3, 4, 5)
 p <- c(0.02, 0.20, 0.30, 0.30, 0.10, 0.08)
 
-## Compute the mean and standard deviation of x
+## Compute the mean and standard deviation of X
 
 mean_x <- sum(x * p)
 var_x <- sum((x - mean_x)^2 * p)
