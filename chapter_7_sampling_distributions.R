2907744b1d6de058a84057d0597b9c5499b7b797
Jeffrey Racine <racinej@mcmaster.ca>
2025-10-23 14:13:40 -0400

    Adding CLT illustration

--- chapter_7_sampling_distributions.R
+++ chapter_7_sampling_distributions.R
@@ -25,3 +25,53 @@ score_table$Relative_Frequency <- score_table$Frequency / sum(score_table$Freque
 # Print this table with only 2 digits after the decimal for the first column
 score_table$Average_Score <- format(round(as.numeric(as.character(score_table$Average_Score)), 2), nsmall = 2)
 print(score_table)
+
+# Central limit theorem illustration
+
+set.seed(42)
+par(mfrow=c(3,2))
+n <- 100
+mean.vec <- numeric()
+
+## X has a chi-square distribution with df = 3
+
+df <- 3
+
+n <- 4
+for(i in 1:100000) mean.vec[i] <- mean(rchisq(n,df=df))
+mean.vec <- sort(mean.vec)
+
+## Generate a histogram density for the raw data X
+hist(x <- sort(rchisq(100000,df=df)),breaks=50,prob=TRUE,main="Density of raw data",xlab="X")
+## Plot the true PDF
+lines(x,dchisq(x,df=df),lwd=2)
+## Now plot the histogram density and normal density of the 100,000 sample means
+hist(mean.vec,prob=TRUE,breaks=50,main="Density of the sample mean",xlab="Sample Mean",sub=paste("n =",n))
+lines(mean.vec,dnorm(mean.vec,mean(mean.vec),sd(mean.vec)),lwd=2)
+legend("topright",c("Sample Mean Density","Normal Density"),lwd=2,bty="n")
+
+n <- 15
+for(i in 1:100000) mean.vec[i] <- mean(rchisq(n,df=df))
+mean.vec <- sort(mean.vec)
+
+## Generate a histogram density for the raw data X
+hist(x <- sort(rchisq(100000,df=df)),breaks=50,prob=TRUE,main="Density of raw data",xlab="X")
+## Plot the true PDF
+lines(x,dchisq(x,df=df),lwd=2)
+## Now plot the histogram density and normal density of the 100,000 sample means
+hist(mean.vec,prob=TRUE,breaks=50,main="Density of the sample mean",xlab="Sample Mean",sub=paste("n =",n))
+lines(mean.vec,dnorm(mean.vec,mean(mean.vec),sd(mean.vec)),lwd=2)
+legend("topright",c("Sample Mean Density","Normal Density"),lwd=2,bty="n")
+
+n <- 80
+for(i in 1:100000) mean.vec[i] <- mean(rchisq(n,df=df))
+mean.vec <- sort(mean.vec)
+
+## Generate a histogram density for the raw data X
+hist(x <- sort(rchisq(100000,df=df)),breaks=50,prob=TRUE,main="Density of raw data",xlab="X")
+## Plot the true PDF
+lines(x,dchisq(x,df=df),lwd=2)
+## Now plot the histogram density and normal density of the 100,000 sample means
+hist(mean.vec,prob=TRUE,breaks=50,main="Density of the sample mean",xlab="Sample Mean",sub=paste("n =",n))
+lines(mean.vec,dnorm(mean.vec,mean(mean.vec),sd(mean.vec)),lwd=2)
+legend("topright",c("Sample Mean Density","Normal Density"),lwd=2,bty="n")
